1bc85efd467e35b143580d7bb30f7441a0f39fdc
godspeed7 <pawelpikus7@gmail.com>
2022-04-20 16:59:15 +0200

    Typesense adapter

---
+++ components/Hit.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = { hit: any };
+
+const Hit = ({ hit }: Props) => {
+  return (
+    <div className="bg-white">
+      <img src={hit.imageSrc} alt={hit.name} />
+      <div className="p-2 border-t border-gray-100">
+        <p className="block text-lg text-gray-900 truncate">{hit.name}</p>
+        <p className="block text-sm font-medium text-gray-500">
+          {hit.category}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hit;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Products from "../components/ProductCollection";
-import clsx from "clsx";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
-
-const navigation = [
-  { name: "Products", href: "#", current: true },
-  { name: "Wishlist", href: "#", current: false },
-];
+import Hit from "~/components/Hit";
 
 const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   server: {
@@ -30,47 +24,15 @@ const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
 });
 const searchClient = typesenseInstantsearchAdapter.searchClient;
 
-const Home: React.FC = () => {
+const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="bg-white shadow-sm">
-        <div className="px-8 mx-auto shadow-sm max-w-7xl">
-          <div className="flex justify-between h-16">
-            <div className="flex">
-              <div className="flex space-x-8">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={clsx(
-                      item.current
-                        ? "border-indigo-500 text-gray-900"
-                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                      "inline-flex items-center px-2 border-b-2 text-sm font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+    <div className="bg-gray-100 ">
       <div className="py-10">
-        <header className="mb-4">
-          <div className="px-8 mx-auto max-w-7xl">
-            <h1 className="text-3xl font-bold leading-tight text-gray-900">
-              Dashboard
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="px-8 mx-auto max-w-7xl">
-            <Products />
-          </div>
-        </main>
+        <InstantSearch indexName="products" searchClient={searchClient}>
+          <SearchBox />
+
+          <Hits hitComponent={Hit} />
+        </InstantSearch>
       </div>
     </div>
   );
