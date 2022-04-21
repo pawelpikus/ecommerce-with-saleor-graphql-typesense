3a28c4632935ee9c10af3b3ae01cd322dcf33b4f
godspeed7 <pawelpikus7@gmail.com>
2022-04-21 09:45:50 +0200

    Add image domains to Next.config.js

--- components/Hit.tsx
+++ components/Hit.tsx
@@ -6,12 +6,13 @@ type Props = { hit: any };
 const Hit = ({ hit }: Props) => {
   return (
     <div className="bg-white">
-      <img src={hit.imageSrc} alt={hit.name} />
+      <Image src={hit.imageSrc} width={16} height={9} alt={hit.name} />
       <div className="p-2 border-t border-gray-100">
         <p className="block text-lg text-gray-900 truncate">{hit.name}</p>
         <p className="block text-sm font-medium text-gray-500">
           {hit.category}
         </p>
+        <p>{hit.price}</p>
       </div>
     </div>
   );
